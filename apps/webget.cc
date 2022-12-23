fb8111db2df2ffda4989428f79fd7ee0656ba3b4
liuchenxing <liuchenxing@bytedance.com>
2022-12-23 17:56:11 +0800

    Add webget impl

--- apps/webget.cc
+++ apps/webget.cc
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -16,9 +17,27 @@ void get_URL(const string &host, const string &path) {
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
-
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    const Address http_server(host, "http");
+    TCPSocket http_sock;
+    http_sock.connect(http_server);
+    string req_str;
+    req_str += "GET " + path + " HTTP/1.1\r\n";
+    req_str += "Host: " + host + "\r\n";
+    req_str += "Connection: close\r\n";
+    req_str += "\r\n";
+    cerr << "request line: " << req_str << endl;
+    http_sock.write(req_str, true);
+    string buf;
+    while(1) {
+        buf = http_sock.read(BUFSIZ);
+        if (buf.size() > 0) {
+            cout << buf;
+        } else {
+            break;
+        }
+    };
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
