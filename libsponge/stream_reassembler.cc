0b66a7bd1edfce034673cbdf8ba07e728487a56e
liuchenxing <liuchenxing@bytedance.com>
2022-12-28 15:45:08 +0800

    only fail at overlapping

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -23,14 +23,30 @@ void StreamReassembler::push_substring(const string &data, const uint64_t index,
     while (!_list.empty() && _list.front().idx <= _expected_idx) {
         auto node = _list.front();
         if (_expected_idx < node.idx + node.v.size()) {
+            // _unordered_bytes -= node.v.size() - _expected_idx + node.idx;
+            _unordered_bytes -= node.v.size();
             _expected_idx += _output.write(node.v.substr(_expected_idx - node.idx));
         }
         _list.pop_front();
     }
-    if (eof) {
+    _unordered_bytes = 0;
+    bool should_eof = true;
+    size_t wi = _output.bytes_written();
+    for (auto cur = _list.begin(); cur != _list.end(); ++cur) {
+        size_t next_wi = cur->idx + cur->v.size();
+        if (next_wi > wi) {
+            if (next_wi - _output.bytes_read() > _capacity) {
+                next_wi = _output.bytes_read() + _capacity;
+                should_eof = false; // out of capacity so can't eof forever
+            }
+            _unordered_bytes += next_wi - max(cur->idx, wi);
+            wi = next_wi;
+        }
+    }
+    if (eof && should_eof) {
         _eof = true;
     }
-    if (_eof && empty()) {
+    if (_eof && unassembled_bytes() == 0) {
         _output.end_input();
     }
 }
@@ -50,4 +66,5 @@ void StreamReassembler::insert(const std::string &v, size_t idx) {
     } else if (cur->idx > idx) {
         _list.insert(cur, std::move(node));
     } // cur->idx == idx duplicated
+    
 }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <string>
 #include <list>
+#include <iostream>
 
 class Node {
   public:
